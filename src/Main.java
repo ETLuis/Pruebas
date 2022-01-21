d05c787fa8d597dee70669fdfb01fd5e482544b1
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2022-01-21 09:41:51 +0100

    A

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Adrián González Hermida");
         System.out.println("Luis Campañó Goldar");
         System.out.println("Daniel González Gregores");
+        //A
     }
 }
